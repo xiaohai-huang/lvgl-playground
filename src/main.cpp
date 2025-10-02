00935272ec04e8997240ee7bea8f3c06fe21b61b
xiaohai-huang <jeromehuang18@gmail.com>
2025-10-02 23:09:32 +0800

    feat(ui): add remote/on-site radio button functionality
    - Implement radio button group for selecting remote or on-site mode
    - Add custom styles for radio button indicators
    - Create event handler to manage radio button state changes
    - Integrate radio buttons into main page layout
    - Connect radio selection to remote subject observable

--- src/main.cpp
+++ src/main.cpp
@@ -48,6 +48,69 @@ static void stop_valve_event_cb(lv_event_t *e)
   lv_subject_set_int(&opening_subject, 0);
   lv_subject_set_int(&closing_subject, 0);
 }
+// remote/on site radio button
+static lv_style_t style_radio;
+static lv_style_t style_radio_chk;
+static int32_t remote_on_site_active_index = 0;
+
+static void radio_event_handler(lv_event_t *e)
+{
+  int32_t *active_id = (int32_t *)lv_event_get_user_data(e);
+  lv_obj_t *cont = (lv_obj_t *)lv_event_get_current_target(e);
+  lv_obj_t *act_cb = lv_event_get_target_obj(e);
+  lv_obj_t *old_cb = lv_obj_get_child(cont, *active_id);
+
+  /*Do nothing if the container was clicked*/
+  if (act_cb == cont)
+    return;
+
+  lv_obj_remove_state(old_cb, LV_STATE_CHECKED); /*Uncheck the previous radio button*/
+  lv_obj_add_state(act_cb, LV_STATE_CHECKED);    /*Check the current radio button*/
+
+  *active_id = lv_obj_get_index(act_cb);
+
+  if (*active_id == 0) // 远程
+  {
+    lv_subject_set_int(&remote_subject, 1);
+  }
+  else
+  {
+    lv_subject_set_int(&remote_subject, 0);
+  }
+}
+
+static void create_radio_button(lv_obj_t *parent, const char *txt)
+{
+  lv_obj_t *obj = lv_checkbox_create(parent);
+  lv_checkbox_set_text(obj, txt);
+  lv_obj_add_flag(obj, LV_OBJ_FLAG_EVENT_BUBBLE);
+  lv_obj_add_style(obj, &style_radio, LV_PART_INDICATOR);
+  lv_obj_add_style(obj, &style_radio_chk, LV_PART_INDICATOR | LV_STATE_CHECKED);
+}
+
+void remote_on_site_buttons()
+{
+  lv_style_init(&style_radio);
+  lv_style_set_radius(&style_radio, LV_RADIUS_CIRCLE);
+
+  lv_style_init(&style_radio_chk);
+  lv_style_set_bg_image_src(&style_radio_chk, NULL);
+
+  lv_obj_t *container = lv_obj_create(lv_screen_active());
+  lv_obj_set_flex_flow(container, LV_FLEX_FLOW_COLUMN);
+  lv_obj_set_size(container, lv_pct(40), lv_pct(80));
+  lv_obj_add_event_cb(container, radio_event_handler, LV_EVENT_CLICKED, &remote_on_site_active_index);
+  create_radio_button(container, "远程"); // 0
+  create_radio_button(container, "就地"); // 1
+  lv_obj_set_style_border_width(container, 0, 0);
+  lv_obj_set_style_bg_opa(container, LV_OPA_TRANSP, 0);
+
+  /*Make the 就地 index 1 checkbox checked*/
+  lv_obj_add_state(lv_obj_get_child(container, 1), LV_STATE_CHECKED);
+  remote_on_site_active_index = 1;
+
+  lv_obj_align(container, LV_ALIGN_TOP_RIGHT, 0, 0);
+}
 
 void main_page()
 {
@@ -166,6 +229,8 @@ void main_page()
   lv_obj_set_style_text_color(labelFault, lv_color_hex(0xFF0000), LV_PART_MAIN);
   // Subscribe to fault_subject as an Observer
   lv_subject_add_observer_obj(&fault_subject, fault_observer_cb, labelFault, NULL);
+
+  remote_on_site_buttons();
 }
 
 // Observer callback functions
